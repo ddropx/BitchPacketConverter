0bf7ddd64122f719752fa24ce4e72a61b37905d1
Anton Chernyshev <a.dm.chernyshev@gmail.com>
2019-01-07 15:37:58 +0300

    New update

--- BitchPacketConverter/ViewController.swift
+++ BitchPacketConverter/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var countingTextField: UITextField!
     @IBOutlet weak var countTextLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    let currencyPickerArray = ["Доширак", "Ролтон"]
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultCountingLabel: UILabel!
     @IBAction func resultCountingButton(_ sender: UIButton) {
         guard let countingText = countingTextField.text, !countingText.isEmpty, let count = Int(countingText) else { return }
-        let bitchCurrency = 38
-        var currencyResult = count / bitchCurrency
+        let doshirak = 38
+        let rollton = 14
+        var currencyResult = count / doshirak
         let name = "Результат - "
         let myString = String(format: NSLocalizedString("result is bitchpackets", comment: ""), locale: Locale(identifier: "ru_RU"), currencyResult)
         let result = "\(name)\(myString)"
@@ -28,12 +32,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        self.imageView.image = UIImage(named: "doshirak")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyPickerArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyPickerArray[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            self.imageView.image = UIImage(named: "doshirak")
+        case 1:
+            self.imageView.image = UIImage(named: "rolton")
+        default:
+            break
+        }
+    }
+    
 }
